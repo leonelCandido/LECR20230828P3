07e9cab31e38f0b7e772f190d8c1f0d81210d174
leonelramirez634 <leonelramirez634@Outlook.es>
2023-09-17 22:23:08 -0600

    Inicio de la guia 3

---
+++ LECRP3ACC/LECRP3ACC/Controllers/ProtetedController.cs
@@ -0,0 +1,36 @@
+﻿
+using Microsoft.AspNetCore.Authorization;
+
+using Microsoft.AspNetCore.Mvc;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+namespace LECRP3ACC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+
+    [Authorize]
+    public class ProtetedController : ControllerBase
+    {
+
+        static List<object> data = new List<object>();
+        // GET: api/<ProtetedController>
+        [HttpGet]
+        public IEnumerable<object> Get()
+        {
+
+
+            return data;
+        }
+        // POST api/<ProtetedController>
+        [HttpPost]
+        public IActionResult Post(string name, string lastName)
+        {
+
+
+            data.Add(new { name, lastName });
+
+            return Ok();
+        }
+    }
+}
---
+++ LECRP3ACC/LECRP3ACC/Controllers/TestController.cs
@@ -0,0 +1,55 @@
+﻿
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+namespace LECRP3ACC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        // GET: api/<TestController>
+        static List<object> data = new List<object>();
+        
+        [HttpGet]
+
+
+        
+        [AllowAnonymous]
+        public IEnumerable<object> Get()
+        {
+
+
+            return data;
+        }
+
+        // POST api/<TestController>
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Post(string name, string lastName)
+        {
+
+
+            data.Add(new { name, lastName});
+
+            return Ok();
+        }
+
+
+        // DELETE api/<TestController>/5
+
+        [Authorize]
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+
+
+            data = new List<object>();
+
+            return Ok();
+        }
+    }
+}
